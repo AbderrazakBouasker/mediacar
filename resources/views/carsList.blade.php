548cabfe3f578ab3b4f13181515050b760b7f10c
Abderrazak <abderrazakbouasker@gmail.com>
2023-07-10 22:02:50 +0100

    car filter categories wip

--- resources/views/carsList.blade.php
+++ resources/views/carsList.blade.php
@@ -1,8 +1,26 @@
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</head>
+
 <div>
     <form action="" method="get">
         <input name="search" type="text" >
         <label for="search" >search</label>
     </form>
+</div>
+<div>
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown button
+        </button>
+        <ul class="dropdown-menu">
+{{--            @foreach()--}}
+{{--                <li><a class="dropdown-item" href="#">Action</a></li>--}}
+{{--            @endforeach--}}
+
+        </ul>
+    </div>
 </div>
 <a href="/cars/create">Add new car</a>
 @foreach($cars as $car)
